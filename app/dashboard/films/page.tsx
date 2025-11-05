b6388f7e345292c23f1dd5868c34e65b8e2d7cba
Kai Achen <kaicachen@gmail.com>
2025-11-04 20:18:20 -0600

    One more try, this time with inline typing and not overriding PageProps

--- app/dashboard/films/page.tsx
+++ app/dashboard/films/page.tsx
@@ -1,24 +1,20 @@
 import ListFilmsOrdered from '@/app/ui/dashboard/list-films-ordered';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchFilms, fetchFilmsOrdered } from '@/app/lib/data';
+import { fetchFilmsOrdered } from '@/app/lib/data';
 
-interface PageProps {
-  searchParams?: {
-    sort?: 'newest' | 'oldest';
-  };
-}
-
-export default async function Page({ searchParams }: PageProps) {
-  
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { sort?: 'newest' | 'oldest' };
+}) {
   const sortOrder = searchParams?.sort === 'oldest' ? 'oldest' : 'newest';
   const films = await fetchFilmsOrdered(sortOrder);
+
   return (
     <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Films
-      </h1>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Films</h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <ListFilmsOrdered listFilmsOrdered={films}/>
+        <ListFilmsOrdered listFilmsOrdered={films} />
       </div>
     </main>
   );
