6ccf1766393befd2394c275084dc705c8fbd703d
Kai Achen <kaicachen@gmail.com>
2025-11-07 11:15:07 -0600

    Moving content to films page

--- app/dashboard/films/page.tsx
+++ app/dashboard/films/page.tsx
@@ -1,20 +1,22 @@
-import ListFilmsOrdered from '@/app/ui/dashboard/list-films-ordered';
+import ListReviewSummaryOrdered from '@/app/ui/dashboard/list-review-summary-ordered';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchFilmsOrdered } from '@/app/lib/data';
+import { fetchFilmReviewSummaryOrdered, fetchFilmsOrdered } from '@/app/lib/data';
 
 export default async function Page({ searchParams }: any) {
-  const resolvedSearchParams = await searchParams
-  
-  const sortOrder: 'newest' | 'oldest' =
-    resolvedSearchParams?.sort === 'oldest' ? 'oldest' : 'newest';
-
-  const films = await fetchFilmsOrdered(sortOrder);
-
+  const resolvedSearchParams = await searchParams;
+  const sortOrder: 'highest' | 'lowest' =
+      resolvedSearchParams?.sort === 'lowest' ? 'lowest' : 'highest';
+  const validCriteria = ['avg_final_rating', 'like_percentage', 'reviews_count', 'avg_initial_rating', 'dislike_percentage'] as const;
+  const criteriaParam = resolvedSearchParams?.criteria;
+  const sortCriteria =
+      validCriteria.includes(criteriaParam) ? criteriaParam : 'avg_final_rating';
+  const reviews = await fetchFilmReviewSummaryOrdered(sortCriteria, sortOrder);
+  const films = await fetchFilmsOrdered();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Films</h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <ListFilmsOrdered listFilmsOrdered={films} />
+        <ListReviewSummaryOrdered listReviewSummaryOrdered={reviews} listFilmsOrdered={films}/>
       </div>
     </main>
   );
