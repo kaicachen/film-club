2905a137e4463893afd96b407e676a7f7053b7a9
Kai Achen <kaicachen@gmail.com>
2025-11-12 11:16:35 -0600

    Fixed weird mobile spacing on logo

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -4,10 +4,10 @@ import { lusitana } from '@/app/ui/fonts';
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white whitespace-nowrap`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[36px]">Film Club</p>
+      <GlobeAltIcon className="h-10 w-10 rotate-[15deg] md:h-12 md:w-12" />
+      <p className="ml-2 text-[28px] md:text-[36px]">Film Club</p>
     </div>
   );
 }
