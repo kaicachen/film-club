9fefc86655a05cf1d8dbc737fa8d1ceda8d8f245
Kai Achen <kaicachen@gmail.com>
2025-11-12 11:19:49 -0600

    Lost the globe icon

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -7,10 +7,10 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        className="mb-2 flex h-20 items-center justify-start rounded-md bg-blue-600 p-4 md:h-32"
         href="/"
       >
-        <div className="text-white w-auto md:w-40">
+        <div className="text-white w-auto">
           <AcmeLogo />
         </div>
       </Link>
